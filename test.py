d438515611092fc39f48cb1ff0b417deca1656b8
Thomas Stoeckl <thomas_stoeckl@yahoo.de>
2021-08-11 19:14:37 +0200

    fixed bug

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+# coding=utf-8
+
 import numpy as np
 
+
 print("Hello World!")
+
 a = np.random.randint(0,10)
-if a < 5:
-    print("3 ist kleiner als 5")
+
+if a < 5 :
+    print("Die Zahl " + str(a) + " ist kleiner als 5")
 else :
-    print("Die Zahl ist größer gleich 5")
+        print("Die Zahl " + str(a) +   " ist groesser/gleich 5")
 
